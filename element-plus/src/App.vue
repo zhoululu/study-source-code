95f1317f21df52e9a8e6fdbb9907b232e43d4938
zhoululu <zhoululu@aegis-data.cn>
2023-03-13 07:31:15 +0800

    feat: 增加分割线组件

---
+++ element-plus/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import FlDivider from './components/FlDivider/index.vue'
+</script>
+
+<template>
+  <div>
+    <FlDivider content-position="right">哈哈</FlDivider>
+  </div>
+
+</template>
+
+<style scoped>
+</style>
---
+++ element-plus/src/components/FlDivider/index.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="fl-divider fl-divider--horizonal" style="--fl-border-style: solid">
+        <div v-if="$slots.default && direction === 'horizonal'" :class="['fl-divider__text', `is-${ contentPosition }`]">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue'
+defineOptions({
+    name: 'FlDivider'
+})
+
+const props = defineProps({
+    direction: {
+        type: String as PropType<'horizonal' | 'vertical'>,
+        default: 'horizonal'
+    },
+    contentPosition: {
+        type: String as PropType<'left' | 'center' | 'right'>,
+        default: 'left'
+    }
+})
+</script>
+
+<style lang="scss">
+.fl-divider {
+    position: relative;
+}
+.fl-divider--horizonal {
+    display: block;
+    height: 1px;
+    width: 100%;
+    margin: 24px 0;
+    border-top: 1px var(--fl-border-color) var(--fl-border-style);
+}
+.fl-divider__text {
+    position: absolute;
+    padding: 0 20px;
+    background-color: var(--fl-bg-color);
+    font-weight: 500;
+    color: var(--fl-text-color-primary);
+    &.is-left {
+        left: 20px;
+        transform: translateY(-50%);
+    }
+    &.is-center {
+        left: 50%;
+        transform: translateX(-50%) translateY(-50%);
+    }
+    &.is-right {
+        right: 20px;
+        transform: translateY(-50%);
+    }
+}
+</style>
